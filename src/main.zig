4e5fbe7f0c7fbffc008ad08df3de9b78bb62e588
rishit khandelwal <rishitkh@gmail.com>
2024-07-03 14:02:17 +0530

    remove obsolute commit

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,6 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // const file = try std.fs.cwd().openFile("test.png", .{});
-
-    // const freader = file.reader();
-
-    // var buf_reader = std.io.bufferedReader(freader);
-
     var image = try png.Image.from_file(allocator, "test.png");
     defer image.deinit();
 
